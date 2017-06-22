f7e51c1916c5cd47a315a36e0aff2c24b539a592
NamdevArade <namdev.arade@gmail.com>
2017-06-22 19:44:50 +0530

    Add files via upload
    for db schemas

---
+++ Txn_VechicleInfo table.sql
@@ -0,0 +1,11 @@
+﻿create table Txn_VechicleInfo(
+vechicleId      	serial not null,
+userId int      	not null,
+vechicleName    	varchar(100) NULL,
+CreatedBy      		int,
+CreatedDate       	timestamp,
+LastModifiedBy 		int,
+LastModifiedDate	timestamp,
+CONSTRAINT Txn_VechicleInfo_pkey PRIMARY KEY (vechicleId),
+FOREIGN KEY(userId) REFERENCES Txn_User
+)
