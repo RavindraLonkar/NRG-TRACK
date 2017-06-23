3ae3265bc9aede20b8230b2e5d048944e770e4e1
Ravindra.Lonkar <Ravindra.Lonkar@EPIHost02.Extrapre.com>
2017-06-23 19:19:46 +0530

    Commit for Login Page Added with all theme JS, CSS, and other required files

---
+++ NRG-Welcome/src/main/java/com/nrg/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.nrg.controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.nrg.models.User;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+		
+	@PostMapping("/save")
+	private static void createEmployee()
+	{
+	    final String uri = "http://localhost:8080/NRG-APP/user/save";
+	 
+	    User user = new User();
+	    user.setAge(22);
+	    user.setFirstName("Namdev");
+	    
+	    RestTemplate restTemplate = new RestTemplate();
+	    User result = restTemplate.postForObject( uri, user, User.class);
+	 
+	    System.out.println(result);
+	}
+}
