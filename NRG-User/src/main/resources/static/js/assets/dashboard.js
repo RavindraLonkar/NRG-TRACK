f6ca0aef9bf81daf4acefd6530e39cce02d88f6e
RavindraLonkar <ravi.lonkar888@gmail.com>
2017-07-07 19:09:17 +0530

    map changes

--- NRG-User/src/main/resources/static/js/assets/dashboard.js
+++ NRG-User/src/main/resources/static/js/assets/dashboard.js
@@ -1,7 +1,14 @@
 function myMap() {
-	var mapProp = {
-		center : new google.maps.LatLng(51.5228742, -0.120850),
-		zoom : 5,
+	var myCenter = new google.maps.LatLng(51.508742, -0.120850);
+	var mapCanvas = document.getElementById("googleMap");
+	var mapOptions = {
+		center : myCenter,
+		zoom : 5
 	};
-	var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+	var map = new google.maps.Map(mapCanvas, mapOptions);
+	var marker = new google.maps.Marker({
+		position : myCenter,
+		animation: google.maps.Animation.BOUNCE
+	});
+	marker.setMap(map);
 }
