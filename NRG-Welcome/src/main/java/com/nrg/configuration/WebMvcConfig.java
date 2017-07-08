a1a99da459aa1d4bd373ba1150785b7d297c72dc
Rahul Raje <Rahul Raje@DESKTOP-1U8OLHD>
2017-07-08 21:06:28 +0530

    commit for responsive pages

--- NRG-Welcome/src/main/java/com/nrg/configuration/WebMvcConfig.java
+++ NRG-Welcome/src/main/java/com/nrg/configuration/WebMvcConfig.java
@@ -1,5 +1,9 @@
 package com.nrg.configuration;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.webresources.StandardRoot;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +16,22 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	public BCryptPasswordEncoder passwordEncoder() {
 		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 		return bCryptPasswordEncoder;
+	}
+	
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+	    TomcatEmbeddedServletContainerFactory tomcatFactory = new TomcatEmbeddedServletContainerFactory() {
+	        @Override
+	        protected void postProcessContext(Context context) {
+	            final int cacheSize = 40 * 1024;
+	            StandardRoot standardRoot = new StandardRoot(context);
+	            standardRoot.setCacheMaxSize(cacheSize);
+	            context.setResources(standardRoot); // This is what made it work in my case.
+
+	            logger.info(String.format("New cache size (KB): %d", context.getResources().getCacheMaxSize()));
+	        }
+	    };
+	    return tomcatFactory;
 	}
 
 }
