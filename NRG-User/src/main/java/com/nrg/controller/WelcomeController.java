f2998ccf7ed2b2fc63e1f3f8b43b445e685056e1
namdev.arade <namdev.arade@gmail.com>
2017-07-13 20:44:33 +0530

    Remove loadhome user class.

--- NRG-User/src/main/java/com/nrg/controller/WelcomeController.java
+++ NRG-User/src/main/java/com/nrg/controller/WelcomeController.java
@@ -3,7 +3,6 @@
  */
 package com.nrg.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
