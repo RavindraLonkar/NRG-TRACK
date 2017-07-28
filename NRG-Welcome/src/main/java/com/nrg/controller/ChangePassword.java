5faa99fc888bed21ceb263d4cac2a28e54506fef
namdev.arade <namdev.arade@gmail.com>
2017-07-28 19:44:36 +0530

    added change password page.

--- NRG-Welcome/src/main/java/com/nrg/controller/ChangePassword.java
+++ NRG-Welcome/src/main/java/com/nrg/controller/ChangePassword.java
@@ -1,26 +1,36 @@
-package com.nrg.forgot.password;
+package com.nrg.controller;
 
 import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.nrg.models.User;
 import com.nrg.utils.Response;
 
 @RestController
-public class ForgotPasswordController {
-
+public class ChangePassword {
+	
 	@Value("${FIND_USER_BY_EMAIL}")
 	private String FIND_USER_BY_EMAIL;
 	
 	Response response = new Response();
-	
 
-	@RequestMapping(value = "/passwordrequest", method = RequestMethod.GET)
+	
+	@RequestMapping(value = {"/changepassword" }, method = RequestMethod.GET)
+	public ModelAndView login(HttpServletRequest request) {
+		ModelAndView modelAndView = new ModelAndView();
+		String token=request.getParameter("token");
+		modelAndView.setViewName("changepassword");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/changepassword", method = RequestMethod.POST)
 	public Response saveAndroidServiceData(ServletRequest req) {
 		RestTemplate rs = new RestTemplate();
 		User userData = new User();
