311c86b0dbd3133bbd1efeb7cf94dc84c4a539a9
namdev.arade <namdev.arade@gmail.com>
2017-08-01 19:13:42 +0530

    commit related user profile page.

---
+++ NRG-User/src/main/resources/static/js/assets/user-profile.js
@@ -0,0 +1,66 @@
+$('#image_click').click(function() {
+	$('#file_click').click();
+});
+
+$('#file_click')
+		.change(
+				function() {
+
+					var extensionToCheck = $('#file_click').val().substring(
+							($('#file_click').val().lastIndexOf('.') + 1));
+
+					if (extensionToCheck != '') {
+						if (extensionToCheck == 'jpg'
+								|| extensionToCheck == 'jpeg'
+								|| extensionToCheck == 'png'
+								|| extensionToCheck == 'gif'
+								|| extensionToCheck == 'ioc'
+								|| extensionToCheck == 'bmp'
+								|| extensionToCheck == 'psd'
+								|| extensionToCheck == 'tif'
+								|| extensionToCheck == 'pspimage'
+								|| extensionToCheck == 'thm'
+								|| extensionToCheck == 'yuv'
+								|| extensionToCheck == 'ai'
+								|| extensionToCheck == 'drw'
+								|| extensionToCheck == 'eps'
+								|| extensionToCheck == 'ps'
+								|| extensionToCheck == 'svg'
+
+								|| extensionToCheck == 'JPG'
+								|| extensionToCheck == 'JPEG'
+								|| extensionToCheck == 'PNG'
+								|| extensionToCheck == 'GIF'
+								|| extensionToCheck == 'IOC'
+								|| extensionToCheck == 'BMP'
+								|| extensionToCheck == 'PSD'
+								|| extensionToCheck == 'TIF'
+								|| extensionToCheck == 'PSPIMAGE'
+								|| extensionToCheck == 'THM'
+								|| extensionToCheck == 'YUV'
+								|| extensionToCheck == 'AI'
+								|| extensionToCheck == 'DRW'
+								|| extensionToCheck == 'EPS'
+								|| extensionToCheck == 'PS'
+								|| extensionToCheck == 'SVG') {
+							$('#photoFileName').html(
+									$('#file_click').val().substring(
+											$('#file_click').val().lastIndexOf(
+													'\\') + 1));
+
+							isImageSelected = 1;
+							$('#isphotoattached').val('1');
+						} else {
+							b(
+									"Error",
+									"Please select a valid Image Extension like .JPG, .PNG, .JPEG, .BMP",
+									"error", doNothing);
+							$('#file_click').val('');
+							$('#photoFileName').val('');
+							isImageSelected = 0;
+							$('#isphotoattached').val('0');
+						}
+					} else {
+						location.reload();
+					}
+				});
