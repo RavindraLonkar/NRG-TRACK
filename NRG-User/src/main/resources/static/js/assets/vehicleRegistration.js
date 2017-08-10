e4d198320a597d7947ab61e0a3508b1f5163e087
RavindraLonkar <ravi.lonkar888@gmail.com>
2017-08-10 19:15:32 +0530

    js seperated for vehicle registration

--- NRG-User/src/main/resources/static/js/assets/vehicleRegistration.js
+++ NRG-User/src/main/resources/static/js/assets/vehicleRegistration.js
@@ -1,41 +1,222 @@
+var table;
 $(document).ready(function() {
-	var data = null;
-	var objectName = "VEHICLE";
-	var dataTableHeader = tableHeaderObject.VEHICLE_HEADERS;
-	var dataTableColumns = tableColumnObject.VEHICLE_COLUMNS;
-	var tableName = "VEHICLE";
-
-	ajaxPost(data, objectName, dataTableHeader, tableColumnObject, tableName)
+	table = $('#vehicleDt').DataTable({
+        "dom": 'Bfrtip',
+		"processing" : true,
+		"scrollX": true,
+        "bDestroy": true,
+        "bLengthChange": false,
+        "select": true,
+		"ajax" : {
+			"url" : $("#contextPath").val() + "/vehicle/list",
+			"type" : "GET",	
+			 "error": function(){  
+	            jQuery("#tablename").append('<tbody class="grid-error"><tr><th colspan="2">No results.</th></tr></tbody>');
+	            jQuery("#tablename_processing").css("display","none");
+	        }
+		},
+		"language": {
+	        infoEmpty: "No records available - Got it?",
+	    },
+		"rowId": 'vehicleid',
+		"columns" : [ {
+            "render": function ( data, type, row ) {
+                return "<a href='#editVehicleModal' data-toggle='modal'><span class = 'glyphicon glyphicon-edit' id='" 
+                + row.vehicleid + "' onclick='setModelData(this.id)'></span></a>";
+            },
+            "targets": 1
+        }, {
+			"data" : "vechiclenumber"
+		}, {
+			"data" : "vechiclename"
+		}, {
+            "render": function ( data, type, row ) {
+                return "<a href='#'><span class = 'glyphicon glyphicon-trash' id='" + row.vehicleid + "' onclick='confirmdeleteVehicle(this.id);'></span></a>";
+            },
+            "targets": 4
+        }],
+        "buttons": [
+            {
+                text: 'Add Vehicle',
+                action: function ( e, dt, node, config ) {
+                	addclassHide();
+                	$('#addVehicleModal').modal('show');
+                }
+            },
+            {
+                text: 'Delete Selected Vehicles',
+                action: function ( e, dt, node, config ) {
+                	confirmdeleteVehicles();
+                }
+            }
+        ]
+	});
 });
 
-function ajaxPost(data, objectName, dataTableHeader, dataTableColumns,
-		tableName) {
+function addVehicle() {
+	var validation = false;
+	var validation = addVehicleRegFormValidation();
+	if (validation == false)
+		return;
 
-	//var url = /*[[@{/vehicle/list}]]*/ 'list';
+	var data = {
+		vechiclenumber : $("#addVechicleNumber").val(),
+		vechiclename : $("#addVechicleName").val()
+	}
 	$.ajax({
-		type : "GET",
-		url : "[[@{/vehicle/list}]]/list",
-		data : data,
-		processData : false, // prevent jQuery from automatically
-								// transforming the data into a query string
-		contentType : false,
+		type : "POST",
+		url : $("#contextPath").val() + "/vehicle/save",
+		data : JSON.stringify(data),
+		processData : false, //prevent jQuery from automatically transforming the data into a query string
+		contentType : 'application/json',
 		cache : false,
 		timeout : 600000,
 		success : function(result) {
 			if (result.status == '1') {
-				var dataSet = result.data;
-				if (jQuery.isEmptyObject(dataSet))
-					return;
-				DataTableHeaderBind(dataTableHeader, objectName);
-				datasetToDataTable(dataSet, objectName,
-						dataTableColumnsBind(dataTableColumns), tableName);
-				BootstrapDialog.alert(result.resonCode);
+				$('#addVehicleModal').modal('hide');
+				bootMessage('sucess', result.resonCode);
+				table.ajax.reload();
 			} else {
-				BootstrapDialog.alert(result.resonCode);
+				bootMessage('Error', result.resonCode);
 			}
 		},
 		error : function(e) {
-			$("#" + objectName + "").hide();
+			console.log("ERROR: ", e);
+		}
+	});
+
+}
+
+function setModelData(vehicleid) {
+	var data = table.row('#' + vehicleid).data();
+	$("#editVehicleId").val(data.vehicleid);
+	$("#editVechicleNumber").val(data.vechiclenumber);
+	$("#editVechicleName").val(data.vechiclename);
+}
+
+function editVedhicle() {
+	var validation = false;
+	var validation = editVehicleRegFormValidation();
+	if (validation == false)
+		return;
+	var data = {
+		vehicleid : $("#editVehicleId").val(),
+		vechiclenumber : $("#editVechicleNumber").val(),
+		vechiclename : $("#editVechicleName").val()
+	}
+	$.ajax({
+		type : "PUT",
+		url : $("#contextPath").val() + "/vehicle/update",
+		data : JSON.stringify(data),
+		processData : false, //prevent jQuery from automatically transforming the data into a query string
+		contentType : 'application/json',
+		cache : false,
+		timeout : 600000,
+		success : function(result) {
+			if (result.status == '1') {
+				$('#editVehicleModal').modal('hide');
+				bootMessage('sucess', result.resonCode);
+				table.ajax.reload();
+			} else {
+				bootMessage('Error', result.resonCode);
+			}
+		},
+		error : function(e) {
+			console.log("ERROR: ", e);
+		}
+	});
+
+}
+
+function confirmdeleteVehicle(vehicleid) {
+	var data = new Array();
+	data.push(table.row('#' + vehicleid).data());
+	
+	if(!(data.length > 0)){
+		bootMessage('Confirm','Please select vehicle to delete');
+		return;
+	} else{
+		BootstrapDialog.show({
+			title : "Confirm",
+			message : "Are you sure! Wants to delete.",
+			buttons : [ {
+				label : "OK",
+				action : function(dialog) {
+					dialog.close();
+					deleteVehicles(data);
+				},
+			}]
+		});
+	}
+}
+
+function confirmdeleteVehicles() {
+	var data = new Array();
+	table.rows('.selected').every(function(rowIdx, tableLoop, rowLoop) {
+		data.push(this.data());
+	});
+	
+	if(!(data.length > 0)){
+		bootMessage('Confirm','Please select vehicle to delete');
+		return;
+	}else{
+		BootstrapDialog.show({
+			title : "Confirm",
+			message : "Are you sure! Wants to delete.",
+			buttons : [ {
+				label : "OK",
+				action : function(dialog) {
+					dialog.close();
+					deleteVehicles(data)							
+				},
+			}]
+		});
+	}
+}
+
+function deleteVehicle(data){
+	$.ajax({
+		type : "DELETE",
+		url : $("#contextPath").val() + "/vehicle/delete",
+		data : JSON.stringify(data),
+		processData : false, //prevent jQuery from automatically transforming the data into a query string
+		contentType : 'application/json',
+		cache : false,
+		timeout : 600000,
+		success : function(result) {
+			if (result.status == '1') {
+				bootMessage('sucess', result.resonCode);
+				table.ajax.reload();
+			} else {
+				bootMessage('Error', result.resonCode);
+			}
+		},
+		error : function(e) {
+			console.log("ERROR: ", e);
+		}
+	});
+
+}
+
+function deleteVehicles(data) {			
+
+	$.ajax({
+		type : "DELETE",
+		url : $("#contextPath").val() + "/vehicle/delete",
+		data : JSON.stringify(data),
+		processData : false, //prevent jQuery from automatically transforming the data into a query string
+		contentType : 'application/json',
+		cache : false,
+		timeout : 600000,
+		success : function(result) {
+			if (result.status == '1') {
+				bootMessage('sucess', result.resonCode);
+				table.ajax.reload();
+			} else {
+				bootMessage('Error', result.resonCode);
+			}
+		},
+		error : function(e) {
 			console.log("ERROR: ", e);
 		}
 	});
